ab2a3f9098ffcc97d8e218f22b83f5557723da81
zinkei32 <distemper.11@gmail.com>
2023-04-30 21:26:04 +0300

    init main

---
+++ UIKit/UIKit/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  UIKit
+//
+//  Created by Сережа on 30.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        print("Hello")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
